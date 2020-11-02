ea83a5514f9c12de436b3fbbcd0fdb0e32e99355
Ganenco Dmitri <ganenko.dmitri@gmail.com>
2020-11-03 00:30:28 +0200

    Update timer.js

---
+++ src/js/timer.js
@@ -0,0 +1,52 @@
+const refs = {
+    daysEl: document.querySelector('span[data-value="days"]'),
+    hoursEl: document.querySelector('span[data-value="hours"]'),
+    minsEl: document.querySelector('span[data-value="mins"]'),
+    secsEl: document.querySelector('span[data-value="secs"]'),
+}
+
+class CountdownTimer {
+  constructor({ targetDate } = {}) {
+      this.targetDate = targetDate;
+      this.init();
+  }
+ 
+    init() {
+        this.getDeltaTime();
+        setInterval(() => {
+         this.getDeltaTime();
+        }, 1000);
+    }
+
+    getDeltaTime() {
+        const currentTime = Date.now();
+        const deltaTime = this.targetDate - currentTime;  
+        this.getTimeComponents(deltaTime);
+    }
+
+    getTimeComponents(time) {
+        const days = Math.floor(time / (1000 * 60 * 60 * 24));
+        const hours =  this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+        const mins =this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+        const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+    this.updateClockFace(days, hours, mins, secs);
+    }
+
+
+    pad(value) {
+     return String(value).padStart(2, '0');
+  }
+
+    updateClockFace(days, hours, mins, secs) {
+    refs.daysEl.textContent = `${days}`;
+    refs.hoursEl.textContent = `${hours}`;
+    refs.minsEl.textContent = `${mins}`;
+    refs.secsEl.textContent = `${secs}`;
+  }
+}
+
+const timer = new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date(2021, 1, 15, 0, 0, 0, 0),
+});
+
